636b3d7a5647e05d194639bf2c4b9c676a4a5299
Franco Liptak <franco.liptak@despegar.com>
2019-12-09 21:49:50 -0300

    Se crea un primer componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Alert from './components/alert/alert';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alert text="My first custom react component"></Alert>
     </div>
   );
 }
